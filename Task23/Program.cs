020fa2e6d4a1db9ffa76665cdc1adbb6053f0920
Dzuder <dzuder14@yandex.ru>
2022-12-25 21:05:18 +0500

    Created task about table of number cubes

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+WriteLine("Enter a number to output a table cubes from 1 to: ");
+int.TryParse(ReadLine(), out int n);
+int i = 1;
+while (i<= n)
+{
+    WriteLine($"cube numbers {i} = {Math.Pow(i, 3)}");
+    i++;
+}
