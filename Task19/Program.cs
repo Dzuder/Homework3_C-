a488ba53d4215fe9a49be9429061ac74ac945ce2
Dzuder <dzuder14@yandex.ru>
2022-12-25 20:16:10 +0500

    Creted task about palindrome a five-digit number

---
+++ Task19/Program.cs
@@ -0,0 +1,4 @@
+﻿using static System.Console;
+WriteLine("Enter a five-digit number: ");
+int.TryParse(ReadLine(), out int num);
+WriteLine(num % 10 == num / 10000 && (num % 100) / 10 == (num / 1000) % 10? "This number is palindrome." : "This number is not palindrome.");
